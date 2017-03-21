1a88f82d6e1ee64a8889f31bf49514e8c93e4817
Sverrir Á. Berg <sab@keilir.com>
2017-03-21 23:28:58 +0000

    Removed profiles, added default
    Save with --default
    Show changed env variables from default (if set)

--- envirou.py
+++ envirou.py
@@ -19,16 +19,16 @@ _CONSOLE_COLORS = {
 _CONFIG_ENV = "ENVIROU_HOME"
 _CONFIG_PATH = "~/.envirou"
 _CONFIG_FILE = "config"
+_DEFAULT_FILE = "default.envirou"
 _SECTION_GROUPS = "groups"
 _SECTION_CUSTOM = "custom"
 _SECTION_HIGHLIGHT = "highlight"
-_SECTION_PROFILE = "profile"
 _NOGROUP = "NA"
 
 _verbose_level = 0
 _groups = defaultdict(list)
 _highlight = {}
-_profiles = defaultdict(list)
+_default = {}
 _stdout = None
 
 
@@ -65,13 +65,22 @@ def output_group(group):
 
 def output_key(k, maxlen):
     fmt = "{key:<{maxlen}} {value}"
-    if k in _highlight:
+    if _default and ((k in _default and os.environ.get(k, "") != _default[k]) or k not in _default):
+        fmt = "{c-red}" + fmt + "{c-end}"
+    elif k in _highlight:
         fmt = "{c-" + _highlight.get(k) + "}" + fmt + "{c-end}"
-    verbose(fmt, key=k, value=os.environ[k], maxlen=maxlen)
+    verbose(fmt, key=k, value=os.environ.get(k, ""), maxlen=maxlen)
+
+
+def clean_split(s, sep="="):
+    very_verbose(s)
+    k, v = s.split(sep, 1)
+    return k.strip(), v.strip()
 
 
 def read_config():
-    folder = os.environ.get("ENVIROU_HOME", "")
+    # Write/prepare first time configuration.
+    folder = os.environ.get(_CONFIG_ENV, "")
     if len(folder.strip()) == 0:
         folder = os.path.expanduser(_CONFIG_PATH)
     if not os.path.isdir(folder):
@@ -89,6 +98,7 @@ def read_config():
         with open(config, "w") as f:
             f.write(default_config)
 
+    # Read config file
     with open(config, "r") as f:
         section = "(none)"
         for l in f.readlines():
@@ -98,28 +108,42 @@ def read_config():
             if l[0] == "[" and l[-1] == "]":
                 section = l[1:-1]
                 continue
-            key, value = l.split("=", 1)
-
+            key, value = clean_split(l)
             if section == _SECTION_GROUPS or section == _SECTION_CUSTOM:
                 for env in value.split(","):
                     very_verbose(_SECTION_GROUPS, key, env)
                     _groups[key].append(env.strip())
             elif section == _SECTION_HIGHLIGHT:
                 for env in value.split(","):
-                    _highlight[env] = key
-            elif section.startswith(_SECTION_PROFILE):
-                _, profile = section.split(":", 1)
-                very_verbose(_SECTION_PROFILE, profile, key, value)
-                _profiles[profile].append(l)
+                    very_verbose(_SECTION_HIGHLIGHT, env, key)
+                    _highlight[env.strip()] = key
             else:
                 very_verbose("Ignoring config item:", section, key, value)
+
+    # Read default environment file
+    default_file = os.path.join(folder, _DEFAULT_FILE)
+    if os.path.exists(default_file):
+        with open(default_file, "r") as f:
+            for l in f.readlines():
+                l = l.strip()   # Trailing LF removed.
+                key, value = l.split("=", 1)
+                very_verbose("reading default env", key, "=", value, ".")
+                _default[key] = value
+
+
+def save_default():
+    folder = os.path.expanduser(_CONFIG_PATH)
+    with open(os.path.join(folder, _DEFAULT_FILE), "w") as f:
+        for k in sorted(os.environ.keys()):
+            f.write("{}={}\n".format(k, os.environ.get(k)))
 
 
 def main(arguments):
     read_config()
 
-    if arguments.reset:
-        very_verbose("resetting standard profile")
+    if arguments.default:
+        very_verbose("saving default environment")
+        save_default()
 
     match_group = defaultdict(list)
     for name, keys in _groups.items():
@@ -148,13 +172,14 @@ def main(arguments):
         else:
             not_displayed_group.append(group)
 
-    if len(not_displayed_group):
-        output_group("mge [-h|-a] {}".format(" ".join(not_displayed_group)))
+    not_currently_set = set(_default.keys()) - set(os.environ.keys())
+    if not_currently_set:
+        output_group("Removed from current env (unset)")
+        for k in sorted(not_currently_set):
+            output_key(k, maxlen)
 
-    if arguments.profile:
-        for l in _profiles[arguments.profile]:
-            key, value = l.split("=", 1)
-            verbose(key, value)
+    if len(not_displayed_group):
+        output_group("Hidden groups: {}".format(" ".join(not_displayed_group)))
 
     # TODO: REMOVE
     shell_eval("export MUCHADOABOUTNOTHING=1")
@@ -164,16 +189,16 @@ def main(arguments):
 
 if __name__ == "__main__":
     redirect_stdout()
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Manage your environment with Envirou!")
+    parser.set_defaults(all=False, default=False)
+
     parser.add_argument("-v", "--verbose", action="count", default=0,
                         help="Increase output verbosity")
 
-    parser.add_argument("-a", "--all", dest="all", action="store_true", help="Show hidden groups")
-    parser.add_argument("--no-all", dest="password", action="store_false", help="Don't show hidden groups (default)")
-    parser.set_defaults(all=False)
+    parser.add_argument("-a", "--all", dest="all", action="store_true", help="Show all groups")
+    parser.add_argument("--no-all", dest="all", action="store_false", help="Don't show hidden groups (default)")
+    parser.add_argument("--default", action="store_true", help="Set current env as default")
 
-    parser.add_argument("-r", "--reset", help="Activate profile")
-    parser.add_argument("-p", "--profile", help="Activate profile")
     parser.add_argument("group", nargs="*", help="Display named group(s)")
 
     args = parser.parse_args()
