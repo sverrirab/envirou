2cf0cc0be66162bee2f4d088f5387d8298d343a6
Sverrir Á. Berg <sab@keilir.com>
2019-10-20 10:36:25 +0000

    curl_install.sh upgrade support
    Prevents running install multiple times

--- curl_install.sh
+++ curl_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+INSTALL_DIR=".envirou"
+upgrade_only=0
 
 function clean_dir() {
     if [ -d $1 ] ; then
@@ -10,15 +12,23 @@ function clean_dir() {
 }
 
 cd
-clean_dir .envirou
+if [ -d "${INSTALL_DIR}" ] ; then
+    upgrade_only=1
+fi
+clean_dir "${INSTALL_DIR}"
 
+cd "${INSTALL_DIR}"
 echo "Downloading Envirou..."
-cd .envirou
 curl -S -s -L -o- https://github.com/sverrirab/envirou/archive/master.tar.gz | tar zx
-
-echo ""
-echo "Installing..."
-./envirou-master/install
-
 cd
+
+if [ ${upgrade_only} -eq 0 ] ; then
+    echo ""
+    echo "Installing..."
+    "${INSTALL_DIR}/envirou-master/install"
+else
+    echo "Upgraded successfully. If Envirou does not work try runnning install again:"
+    echo "~/${INSTALL_DIR}/envirou-master/install"
+fi
+
 rm -rf .envirou.old
